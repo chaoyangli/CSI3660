cb88fc3071c7937386c741f0b3020aaad72bba3b
chaoyangli <33186666+chaoyangli@users.noreply.github.com>
2018-05-21 14:22:43 -0400

    Update cronb.sh

--- cronb.sh
+++ cronb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash                   #the script should be interpreted by bash shell
 while true
 do
-echo what date is today?|/home/chaoyangli/crona.sh             # In crona.sh: #!/bin/bash
+echo what date is today?|/home/chaoyangli/crona.sh             # In crona.sh: #!/bin/bash echo `date`
 sleep 0.5
 done
