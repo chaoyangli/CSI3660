cf2a6504f812d5d782ea82fd4a057bdf6bfd0916
chaoyangli <33186666+chaoyangli@users.noreply.github.com>
2018-05-21 15:08:07 -0400

    Update and rename cronb.sh to crontab-display_every_0.5s.sh

--- crontab-display_every_0.5s.sh
+++ crontab-display_every_0.5s.sh
@@ -2,6 +2,6 @@
 #!/bin/bash                   #the script should be interpreted by bash shell
 while true
 do
-echo what date is today?|/home/chaoyangli/crona.sh             # In crona.sh: #!/bin/bash echo `date`
+echo what date is today?|echo `date`             # In crona.sh: #!/bin/bash echo `date`
 sleep 0.5
 done
