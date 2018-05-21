c78653168a6bed5a336e05f54494313f815909f5
chaoyangli <33186666+chaoyangli@users.noreply.github.com>
2018-05-21 14:21:46 -0400

    Every-0.5-second-display

---
+++ cronb.sh
@@ -0,0 +1,6 @@
+#!/bin/bash                   #the script should be interpreted by bash shell
+while true
+do
+echo what date is today?|/home/chaoyangli/crona.sh             # In crona.sh: #!/bin/bash
+sleep 0.5
+done
